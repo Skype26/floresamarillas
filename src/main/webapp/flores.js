af4ed50e134ab01cc3b6a587735ff255f6ee63c6
03332 <03332@Ray>
2024-09-20 12:29:07 -0500

    falta

--- src/main/webapp/flores.js
+++ src/main/webapp/flores.js
@@ -1,26 +1,24 @@
-// Eliminar la clase 'container' del cuerpo
-window.onload = () => {
-    document.body.classList.remove("container");
-};
-
-// Ejecutar al hacer clic en el botón
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById("btnLogin").addEventListener("click", function () {
-        let login = document.getElementById("nameFriend").value;
+            document.getElementById("btnLogin").addEventListener("click", function () {
+                let login = document.getElementById("nameFriend").value;
+                let audioElement = document.getElementById("audioElement");
+                let audioElementa = document.getElementById("audioElementa");
 
-        // Redirigir dependiendo del nombre ingresado
-        if (login == "Aylin") {
-            window.location.href = "floresA.html";
-        } else if (login == "Madai") {
-            window.location.href = "floresM.html";
-        } else if (login == "Cristina") {
-            window.location.href = "floresC.html";
-        } else if (login == "Silvana") {
-            window.location.href = "floresS.html";
-        } else {
-            // Si el nombre no es ninguno de los anteriores
-            alert("Hazta pa poner tu nombre eres sonsa");
-            //window.location.hre = "error.html"; // Puedes redirigir a una página de error o a otra diferente
-        }
-    });
-});
+                if (login === "Aylin") { // Reproduce el audio Floricienta
+                    setTimeout(function () {
+                        window.location.href = "floresA.html"; // Redirige a la página de Aylin
+                    }, 2000); // Esperar 2 segundos antes de redirigir
+                } else if (login === "Madai") {
+                    //audioElementa.play(); // Reproduce el audio "Mi Corazón"
+                    setTimeout(function () {
+                        window.location.href = "prueba.html"; // Redirige a la página de Madai
+                    }, 2000); // Esperar 2 segundos antes de redirigir
+                } else if (login === "Cristina") {
+                    window.location.href = "floresC.html"; // Redirige a la página de Cristina
+                } else if (login === "Silvana") {
+                    window.location.href = "floresS.html"; // Redirige a la página de Silvana
+                } else {
+                    alert("Hazta pa poner tu nombre eres sonsa");
+                }
+            });
+        });
